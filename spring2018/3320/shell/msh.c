125fdaeb2a8843e9bcf180eb9777e65c4ef012f5
Chris Jakins <chris.fp.jakins@gmail.com>
2018-02-11 18:36:33 -0600

    Implemented basic signal handler

--- spring2018/3320/shell/msh.c
+++ spring2018/3320/shell/msh.c
@@ -11,58 +11,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 #define WHITESPACE " \t\n"
 #define MAX_COMMAND_SIZE 255
 #define MAX_NUM_ARGUMENTS 11
 
 int promptAndReadIn(char **);
+void handleSIGTSTP(int sig);
 
 int main()
 {
-    char * cmdStr = malloc( MAX_COMMAND_SIZE );
+    char ** token = malloc( MAX_NUM_ARGUMENTS );
+    int status;
+    int execErr = 0;
+
+    /* init handler for ctrl-z */
+    struct sigaction tstop;
+    memset(&tstop, '\0', sizeof(tstop));
+    tstop.sa_handler = &handleSIGTSTP;
+    sigaction(SIGTSTP, &tstop, NULL);
 
     while ( 1 )
     {
-        char ** token = malloc( MAX_NUM_ARGUMENTS );
         int tokenCount = promptAndReadIn(token);
-        /* printf("msh> "); */
 
-        /* while ( !fgets(cmdStr, MAX_COMMAND_SIZE, stdin) ) ; */
-
-        /* char * token[MAX_NUM_ARGUMENTS]; */
-        /* int tokenCount = 0; */
-        /* char * argPtr; */
-        /* char * workingStr = strdup( cmdStr ); */
-        /* char * workingRoot = workingStr; */
-
-        /* while ( ( (argPtr = strsep(&workingStr, WHITESPACE)) != NULL ) && */
-        /*           (tokenCount < MAX_NUM_ARGUMENTS) ) */
-        /* { */
-        /*     token[tokenCount] = strndup( argPtr, MAX_COMMAND_SIZE ); */
-        /*     if ( strlen( token[tokenCount] ) == 0 ) */
-        /*     { */
-        /*         token[tokenCount] = NULL; */
-        /*     } */
-        /*     tokenCount++; */
-        /* } */
-
-        /* int tokenIndex = 0; */
-        /* for (tokenIndex = 0; tokenIndex < tokenCount; tokenIndex++) */
-        /* { */
-        /*     printf("token[%d] = %s\n", tokenIndex, token[tokenIndex]); */
-        /* } */
-        /* free( workingRoot ); */
-        int tokenIndex = 0;
-        for (tokenIndex = 0; tokenIndex < tokenCount; tokenIndex++)
+        if (token[0] != NULL && 
+           (strcmp( token[0], "exit" ) == 0 || strcmp( token[0], "quit" ) == 0))
         {
-            printf("token[%d] = %s\n", tokenIndex, token[tokenIndex]);
+            return 0;
         }
+
+        pid_t pid = fork();
+
+        if (pid != 0)
+        {
+            waitpid(pid, &status, 0);
+        }
+        else
+        {
+            char path1[MAX_COMMAND_SIZE] = "/bin/";
+            char path2[MAX_COMMAND_SIZE] = "/usr/bin/";
+            char path3[MAX_COMMAND_SIZE] = "/usr/local/bin/";
+            char path4[MAX_COMMAND_SIZE] = "./";
+
+            char * command = strcat(path1, token[0]);
+            execErr = execve(command, token, 0);
+
+            command = strcat(path2, token[0]);
+            execErr = execve(command, token, 0);
+
+            command = strcat(path3, token[0]);
+            execErr = execve(command, token, 0);
+
+            command = strcat(path4, token[0]);
+            execErr = execve(command, token, 0);
+
+            if (execErr)
+            {
+                printf("%s: Command not found.\n", token[0]);
+            }
+            return 0;
+        }
+
+
+
     }
-    free( cmdStr );
+    free( token );
     return 0;
 }
 
+/*
+    token -> array of strings ending in (null) are stored here after function returns
+    number of tokens is returned
+
+    this code taken from Trevor Bakker :
+    https://github.com/CSE3320/Shell-Assignment/blob/master/mfs.c
+*/
 int promptAndReadIn(char ** token)
 {
     printf("msh> ");
@@ -88,4 +113,9 @@ int promptAndReadIn(char ** token)
 
     free( workingRoot );
     return tokenCount;
+}
+
+void handleSIGTSTP(int sig)
+{
+    printf("Hello %d\n", sig);
 }
