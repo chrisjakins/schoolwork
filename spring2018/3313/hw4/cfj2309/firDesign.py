229dc3b2ec92b81c47d3dd5d979b59ab6315fdac
Chris Jakins <cjakins12@yahoo.com>
2018-02-27 10:05:55 -0600

    Implement low pass filter

--- spring2018/3313/hw4/cfj2309/firDesign.py
+++ spring2018/3313/hw4/cfj2309/firDesign.py
@@ -1,14 +1,34 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
-filename = "data-filtering.csv"
-sampleRate = 2000
-
-
-def lowpass() :
+def lowpass(signal) :
     cutoff = 50
-    print("hello")
+    filterLength = 21
+    M = filterLength - 1
+    normCutoff = cutoff / sampleRate
+
+    filterWeights = []
+    for n in range(0, filterLength) :
+        if n == M / 2 :
+            filterWeights.append(2 * normCutoff)
+        else :
+            filterWeights.append(
+                np.sin(2 * np.pi * normCutoff * (n - M / 2)) / (np.pi * (n - M / 2))
+            )
+    print(filterWeights)
 
 
 
 def highpass() :
     print("Hello")
+
+
+
+
+
+filename = "data-filtering.csv"
+sampleRate = 2000
+
+data = np.genfromtxt(filename, delimiter=',')
+
+lowpassData = lowpass(data)
