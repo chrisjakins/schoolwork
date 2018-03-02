b57e08d7d8391eae6a55b7c4c098c7a327b15c2b
Chris Jakins <cjakins12@yahoo.com>
2018-03-01 21:39:55 -0600

    Add some basic i/o

--- spring2018/3313/hw5/cfj2309/dtmf.py
+++ spring2018/3313/hw5/cfj2309/dtmf.py
@@ -6,17 +6,48 @@
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import freqz
 
-# SETUP
+#   SETUP
 
 filename = "tones.csv"
 sampleRate = 8000
+numSamples = 4000
 filterLength = 64
 
+frequencyBank = np.array([697, 770, 852, 941, 1209, 1336, 1477])
+filterBank = []
+outputs = []
+
+#   FUNCTIONS
+
+def setupFilters() :
+    for freq in frequencyBank :
+        interim = []
+        for n in range(0, filterLength) :
+            interim.append(
+                2 / filterLength * np.cos(2 * np.pi * freq * n / sampleRate)
+            )
+        filterBank.append(interim)
 
 
 
 
+#
+#   EFFECTIVE MAIN
+#
 
+data = np.genfromtxt(filename, delimiter = ',')
+setupFilters()
 
-data = np.genfromtxt(filename)
+for f in filterBank :
+    outputs.append(np.convolve(f, data))
+
+print(outputs)
+
+"""
+x, y = freqz(filterBank[0], 1) 
+plt.plot(x, abs(y))
+plt.show()
+"""
