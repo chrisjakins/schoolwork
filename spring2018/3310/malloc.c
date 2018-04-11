aaac3e41a44b68d7362d3865438ea38f1c1c5561
Chris Jakins <cjakins12@yahoo.com>
2018-04-11 15:44:54 -0500

    Added some initial utility and data structure to malloc implementation. Needs to be reworked...

--- spring2018/3310/malloc.c
+++ spring2018/3310/malloc.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 
-static char X[1000];
+typdef struct Map {
+    static char heap[1000];
+    static char lookup[1000];
+} Map;
+
+Map Heap;
+
+// Utility methods
+// Required methods
 
 void * _malloc(size_t size) {
 
@@ -8,7 +16,11 @@ void * _malloc(size_t size) {
 }
 
 void _free(void * ptr) {
+    int numBlocks = *(ptr - 1);
 
+    // see if next or previous chain of blocks is used
+    //      combine if necessary
+    // set the data blocks to 0
 }
 
 void * _calloc(size_t nmemb, size_t size) {
@@ -18,3 +30,4 @@ void * _calloc(size_t nmemb, size_t size) {
 void * realloc(void * ptr, size_t size) {
 
 }
+
