8c1f77e2afa6df6534163a5651b97660f49e4d8b
Chris Jakins <chris.fp.jakins@gmail.com>
2018-04-10 19:35:17 -0500

    Mostly? finished shelving assignment

--- spring2018/3313/hw8/cfj2309/shelvingBass.py
+++ spring2018/3313/hw8/cfj2309/shelvingBass.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 
+#
 #   setup
+#
 configFile = "shelvingConfig.txt"
+outfile = "shelvingOutput.wav"
 
+#   defining these for later
+infile = ""
+gain = 0
+cutoff = 0
 
-
-
-
-
-
-
-
+#
+#   Functions
+#
 
 #
 #   effective main
@@ -21,13 +24,13 @@ config = open(configFile, "r")
 args = config.read().split("\n")
 
 infile = args[0]
-data, sampleRate = sf.read(infile)
-fundamental = sampleRate / len(data)
 gain = int(args[1])
 cutoff = int(args[2])
 
-freqData = abs(np.fft.fft(data))
+data, sampleRate = sf.read(infile)
 
+# setting up needed variables
+fundamental = sampleRate / len(data)
 
 mu = 10**(gain / 20)
 thetaC = 2 * np.pi * cutoff / sampleRate
@@ -39,13 +42,45 @@ gamma = num / den
 alpha = (1 - gamma) / 2
     
 
+# this process taken from DSP First
+outData = []
+for i in range(len(data)):
+    if i == 0:
+        outData.append(alpha * (data[0]))
+    else:
+        outData.append(
+                alpha * (data[i] + data[i - 1]) + gamma * outData[i - 1]
+                )
 
+finalData = []
+for i in range(len(outData)):
+    finalData.append(data[i] + (mu - 1) * outData[i])
 
 #
 #   plotting
 #
 
+freqData = abs(np.fft.fft(data))
+finalData = abs(np.fft.fft(finalData))
+
+maxVal = np.amax(freqData)
+#   generate the frequencies for corresponding bins in dft
 xaxis = [fundamental * x for x in range(len(freqData) // 4)]
 
+plt.subplot(1, 2, 1)
 plt.plot(xaxis, freqData[:len(freqData) // 4])
+ymin, ymax = plt.ylim()
+plt.ylim([ymin, maxVal + 100])
+
+plt.subplot(1, 2, 2)
+plt.plot(xaxis, finalData[:len(finalData) // 4])
+plt.ylim([ymin, maxVal + 100])
+
+plt.tight_layout()
 plt.show()
+
+#
+#   write out modified soundfile
+#
+
+sf.write(outfile, finalData, sampleRate)
