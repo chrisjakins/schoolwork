e287353227c73e026076b10d8c19a241312623ad
Chris Jakins <chris.fp.jakins@gmail.com>
2018-04-14 17:30:47 -0500

    finish musicID, moved jpgs around for testing

--- spring2018/3313/hw9/cfj2309/musicID.py
+++ spring2018/3313/hw9/cfj2309/musicID.py
@@ -17,14 +17,32 @@ import glob
 testFilename = "testSong.wav"
 pattern = "song-*.wav"
 
+songDB = {}
+
+
+#
+#   Functions
+#
+
+def calcSignature(data, fs):
+    f, t, sxx = spectrogram(data, fs = fs, nperseg = int(.5 * fs))
+    return f[np.argmax(sxx, axis = 0)]
+
+#
+#   Main
+#
+
+
 testData, testSampleRate = sf.read(testFilename)
+testSignature = calcSignature(testData, testSampleRate)
+testNorm = np.linalg.norm(testSignature, ord = 1)
+
 songs = glob.glob(pattern)
-signatures = [] 
-
-results = []
-
-
 
 for f in songs:
     data, sampleRate = sf.read(f)
-    f, t, sxx = spectrogram(data, fs = sampleRate, nperseg = int(.5 * sampleRate))
+    signature = calcSignature(data, sampleRate)
+    songDB[f] = np.linalg.norm(signature - testSignature, ord = 1)
+
+for key, value in sorted(songDB.items(), key = lambda x: x[1]):
+    print("%.1f %s" % (value, key))
