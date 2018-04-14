6807ee9e4c19f6f9fe2cb31d6718742ec5c60fd4
Chris Jakins <chris.fp.jakins@gmail.com>
2018-04-13 23:54:27 -0500

    added initial setup for musicID assignment

--- spring2018/3313/hw9/cfj2309/musicID.py
+++ spring2018/3313/hw9/cfj2309/musicID.py
@@ -8,5 +8,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
 import soundfile as sf
+import glob
+
+#
+#   Setup
+#
 
 testFilename = "testSong.wav"
+pattern = "song-*.wav"
+
+testData, testSampleRate = sf.read(testFilename)
+songs = glob.glob(pattern)
+signatures = [] 
+
+results = []
+
+
+
+for f in songs:
+    data, sampleRate = sf.read(f)
+    f, t, sxx = spectrogram(data, fs = sampleRate, nperseg = int(.5 * sampleRate))
