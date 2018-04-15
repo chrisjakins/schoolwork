ed0e9ce7d5f6a8e1e2e38ccc23dfadcfd9ce8da1
Chris Jakins <chris.fp.jakins@gmail.com>
2018-04-15 13:03:30 -0500

    Implementation finished, may need a touchup

--- spring2018/3310/malloc.c
+++ spring2018/3310/malloc.c
@@ -1,27 +1,139 @@
 #include <stdlib.h>
+#include <string.h>
+
+char heap[10000000];
+
+typedef struct list {
+    int size;
+    int free;
+    struct list * next;
+} List;
+
+
+List * freeList = (void *) heap;
 
 // Utility methods
+void init() {
+    freeList->size = 10000000 - sizeof(List);
+    freeList->free = 1;
+    freeList->next = NULL;
+}
+
+void split(List * fitting_slot, int size) {
+    List * new = ((void *) fitting_slot + size + sizeof(List));
+    new->size = (fitting_slot->size) - size - sizeof(List);
+    new->free = 1;
+    new->next = fitting_slot->next;
+    fitting_slot->size = size;
+    fitting_slot->free = 0;
+    fitting_slot->next = new;
+}
+
+void merge() {
+    List * curr, * prev;
+    curr = freeList;
+    while (curr->next) {
+        if (curr->free && curr->next->free) {
+            curr->size += curr->next->size + sizeof(List);
+            curr->next = curr->next->next;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
 // Required methods
 
-void * _malloc(size_t size) {
+void * _malloc(int numBytes) {
+    List * curr, * prev;
+    void * result;
+    if (!(freeList->size)) {
+        init();
+    }
+    curr = freeList;
+    while ((curr->size < numBytes || curr->free == 0) && curr->next) {
+        prev = curr;
+        curr = curr->next;
+    }
 
-    return &Heap;
+    if (curr->size == numBytes) {
+
+        curr->free = 0;
+        result = (void *) ++curr;
+        return result;
+
+    } else if (curr->size > numBytes + sizeof(List)) {
+
+        split(curr, numBytes);
+        result = (void *) ++curr;
+        return result;
+
+    } else {
+
+        result = NULL;
+        return result;
+
+    }
 }
 
 void _free(void * ptr) {
-    ptr = (Map *) ptr;
-//    char numBlocks = (ptr - 1)->lookup[100];
-
-    // see if next or previous chain of blocks is used
-    //      combine if necessary
-    // set the data blocks to 0
+    if ((void *) heap <= ptr && ptr <= (void * ) (heap + 10000000)) {
+        List * curr = ptr;
+        curr--;
+        curr->free = 1;
+        merge();
+    }
 }
 
-void * _calloc(size_t nmemb, size_t size) {
+// same as malloc
+// but with memsets
+void * _calloc(size_t nmemb, int size) {
+    List * curr, * prev;
+    void * result;
+    if (!(freeList->size)) {
+        init();
+    }
+    curr = freeList;
+    while ((curr->size < size || curr->free == 0) && curr->next) {
+        prev = curr;
+        curr = curr->next;
+    }
 
+    if (curr->size == size) {
+
+        result = (void *) ++curr;
+        memset(result, 0, size);
+        return result;
+
+    } else if (curr->size > size + sizeof(List)) {
+
+        split(curr, size);
+        result = (void *) ++curr;
+        memset(result, 0, size);
+        return result;
+
+    } else {
+
+        result = NULL;
+        return result;
+
+    }
 }
 
 void * realloc(void * ptr, size_t size) {
+    List * curr, * prev;
+    void * result;
+    if (!(freeList->size)) {
+        init();
+    }
+    curr = freeList;
+    while ((curr->size < size || curr->free == 0) && curr->next) {
+        prev = curr;
+        curr = curr->next;
+    }
 
+    curr->size = size;
+    result = (void *) ++curr;
+    return result;
 }
 
