717128c95d34b37df7c1e7e9adee17b1708c3278
Chris Jakins <cjakins12@yahoo.com>
2018-09-16 16:16:14 -0500

    Import data from training file and split off class labels into separate array

--- fall2018/4309/hw1/naive_bayes.py
+++ fall2018/4309/hw1/naive_bayes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-
 import sys
 import os
-import numpy
+import numpy as np
+
 
 if len(sys.argv) != 3:
     print("Usage: python3 naive_bayes.py <training_file> <test_file>")
@@ -10,6 +10,8 @@ if len(sys.argv) != 3:
 
 training_filename = sys.argv[1]
 test_filename = sys.argv[2]
+
+# locating files
 
 def find(name, path):
     for root, dirs, files in os.walk(path, topdown = False):
@@ -23,3 +25,22 @@ if training_file == None:
 test_file = find(test_filename, './')
 if test_file == None:
     test_file = find(test_filename, '/')
+
+# reading files and convert to numpy array
+# split class data off of training data
+
+training_file = open(training_file, 'r')
+training_data = []
+for line in training_file:
+    training_data.append(line.split())
+
+training_data = np.asarray([[float(y) for y in x] for x in training_data])
+training_classes = training_data[:, len(training_data[0]) - 1]
+training_data = training_data[:, : -1]
+print(len(training_data))
+
+"""
+test_file = open(test_file, 'r')
+for line in test_file:
+    print("test")
+"""
