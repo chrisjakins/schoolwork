2ca42e10aed8556718c59f25d7431eda43f23cb9
Chris Jakins <cjakins12@yahoo.com>
2018-09-17 16:10:52 -0500

    Output working, classifier still slightly broken

--- fall2018/4309/hw1/classifier.py
+++ fall2018/4309/hw1/classifier.py
@@ -9,20 +9,76 @@ class NaiveBayesClassifier:
         self.classes = np.unique(self.data_labels)
         self.mean = np.zeros((len(self.classes), len(data[0])))
         self.s_deviation = np.zeros((len(self.classes), len(data[0])))
+        self.class_frequencies = np.asarray([np.count_nonzero(self.data_labels == x) for x in self.classes]) / len(self.data_labels)
 
 
     def train(self):
-        # get mean and standard deviation for each dimension for each class
+        # get mean and standard deviation for each class
         counts = np.zeros((len(self.classes), 1))
         for i in range(0, len(self.data)):
             class_number_index = np.where(self.classes == self.data_labels[i])[0][0]
+            counts[class_number_index] += 1
             self.mean[class_number_index] += self.data[i]
             self.s_deviation[class_number_index] += np.square(self.data[i])
-            counts[class_number_index] += 1
 
-        self.s_deviation -= (np.divide(np.square(self.mean), counts))
-        self.s_deviation /= (counts - 1)
+        self.s_deviation = np.subtract(self.s_deviation, np.divide(np.square(self.mean), counts))
+        self.s_deviation = np.divide(self.s_deviation, np.subtract(counts, 1))
         self.s_deviation = np.sqrt(self.s_deviation)
-        self.s_deviation[self.s_deviation < 0.001] = 0.001
+        self.s_deviation[self.s_deviation < 0.01] = 0.01
         self.mean /= counts # broadcasted
+        return self.classes, self.mean, self.s_deviation
 
+
+    def test(self, test_data):
+        """
+        print("Mean")
+        print(self.mean)
+        print("S_Deviation")
+        print(self.s_deviation)
+        print("Test_data")
+        print(test_data)
+        print(self.class_frequencies)
+        """
+        p_x = 0
+        interim = 0
+        probabilities = []
+        for i in range(0, len(self.classes)):
+            # calculate gaussians for each dimension for the i-th class
+            numerator = np.negative(np.square(np.subtract(test_data, self.mean[i])))
+            denominator = np.multiply(np.square(self.s_deviation[i]), 2)
+            #print("Numerator")
+            #print(numerator)
+            #print("Denominator")
+            #print(numerator)
+            exponential = np.exp(np.divide(numerator, denominator))
+            gaussian_value = np.divide(exponential, np.multiply(self.s_deviation[i], np.sqrt(2 * np.pi)))
+
+            #print("Gaussian")
+            #print(gaussian_value)
+            # correct up to here
+
+            # p(x|Ck) * p(Ck)
+            interim = np.multiply(np.prod(gaussian_value), self.class_frequencies[i])
+            """
+            np.set_printoptions(suppress=True)
+            print(gaussian_value)
+            print("%.5f" % np.prod(gaussian_value))
+            print("interim")
+            print(interim)
+            """
+
+            # add to p(x) (SUM RULE)
+            p_x += interim
+
+            probabilities.append(interim)
+
+        probabilities = np.asarray(probabilities)
+        probabilities = np.divide(probabilities, p_x)
+        probability = probabilities[np.argmax(probabilities)]
+        results = []
+        for i in range(0, len(probabilities)):
+            if probabilities[i] == probability:
+                results.append(self.classes[i])
+
+        #result_class = self.classes[np.argmax(probabilities)]
+        return results, probability
--- fall2018/4309/hw1/naive_bayes.py
+++ fall2018/4309/hw1/naive_bayes.py
@@ -45,8 +45,49 @@ for line in test_file:
     test_data.append([float(x) for x in line.split()])
 
 test_data = np.asarray(test_data)
+test_classes = test_data[:, - 1]
+test_data = test_data[:, : -1]
 
 # naive bayes classifier
 
+# training
+
+def print_training(labels, mean, s_deviation):
+    for i in range(0, len(labels)):
+        for j in range(0, len(mean[i])):
+            print("Class: %d, attribute: %d, mean = %.2f, std = %.2f" % (labels[i], j + 1, mean[i][j], s_deviation[i][j]))
+
+
 cfier = NaiveBayesClassifier(training_data, training_classes)
-cfier.train()
+class_labels, mean, s_deviation = cfier.train()
+
+print_training(class_labels, mean, s_deviation)
+
+# test
+
+def print_test(row, prediction, probability, actual, accuracy):
+    print("ID = %5d, predicted = %3d, probability = %.4f, true = %3d, accuracy = %4.2f\n" % (row, prediction, probability, actual, accuracy))
+
+count = 0
+"""
+predicitions, probability = cfier.test(test_data[1])
+print(predictions)
+print(test_classes[1])
+"""
+total = 0
+for i in range(0, len(test_data)):
+    predictions, probability = cfier.test(test_data[i])
+    if test_classes[i] in predictions:
+        accuracy = 1 / len(predictions)
+        print(accuracy)
+        print_test(i + 1, test_classes[i], probability, test_classes[i], accuracy)
+    else:
+        print_test(i + 1, predictions[0], probability, test_classes[i], 0)
+    
+    total += accuracy
+
+    if predictions == test_classes[i]:
+        count += 1
+
+print("Classification accuracy = %6.4f" % (total / len(test_data)))
+#print(count)
