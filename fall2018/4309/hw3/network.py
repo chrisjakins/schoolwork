4a9982082b349ed2e98936a588bd057f8c7c8f31
Chris Jakins <cjakins12@yahoo.com>
2018-10-18 22:58:22 -0500

    Finalize Neural Network model, 92% accuracy on pendigits dataset. Can generalize to any number of layers or units per layers or training rounds.

--- fall2018/4309/hw3/network.py
+++ fall2018/4309/hw3/network.py
@@ -27,101 +27,81 @@ class NeuralNetwork:
         self.train_data = np.hstack((self.train_data, data)).T
 
         # initialize training classes vector
+        # TODO generalize based off index in labels
         self.num_classes = len(np.unique(labels))
         self.train_labels = np.zeros((self.num_classes, self.num_examples))
         for i in range(self.num_examples):
-            self.train_labels[int(labels[i]) - 1][i] = 1
-        
+            index = np.where(np.unique(labels) == labels[i])[0][0]
+            #self.train_labels[int(labels[i]) - 1][i] = 1
+            self.train_labels[index][i] = 1
+       
 
-    def train(self):
-        # initializing
+    def train_general(self):
         self.model = {}
-        W1 = np.subtract(np.multiply(np.random.rand(self.num_dimensions + 1, self.upl), .1), .05)
-        W2 = np.subtract(np.multiply(np.random.rand(self.upl + 1, self.num_classes), .1), .05)
-        self.model['W1'] = W1
-        self.model['W2'] = W2
+        if self.num_layers == 2:
+            self.model['W1'] = np.subtract(np.multiply(np.random.rand(self.num_dimensions + 1, self.upl), .1), .05)
+        else:
+            self.model['W1'] = np.subtract(np.multiply(np.random.rand(self.num_dimensions + 1, self.upl), .1), .05)
+            for i in range(2, self.num_layers - 1):
+                self.model['W' + str(i)] = np.subtract(np.multiply(np.random.rand(self.upl + 1, self.upl), .1), .05)
+            self.model['W' + str(self.num_layers - 1)] = np.subtract(np.multiply(np.random.rand(self.upl + 1, self.num_classes), .1), .05)
 
+        self.outs = {}
         for i in range(0, self.num_rounds):
+            for j in range(0, self.num_examples): #self.num_examples
+                # forward
+                z = self.train_data[:,j].reshape(self.num_dimensions + 1, 1)
+                self.outs['Z0'] = z
+                for k in range(0, self.num_layers - 2):
+                    a = np.dot(self.model['W' + str(k + 1)].T, self.outs['Z' + str(k)])
+                    z = self.sigmoid(a).reshape(self.upl, 1)
+                    z = np.vstack((np.ones((1,1)), z))
+                    self.outs['Z' + str(k + 1)] = z
 
-            # INPUT LAYER
-            a = np.dot(W1.T, self.train_data[:,0])
-            Z1 = self.sigmoid(a)
-            self.outs = {'Z1' : Z1}
-            print('After input layer -> hidden layer feedforward')
-            print(self.train_data[:,0])
-            print(W1)
-            print(a)
-            print(Z1)
+                # do final layer
+                a = np.dot(self.model['W' + str(self.num_layers - 1)].T, self.outs['Z' + str(self.num_layers - 2)])
+                self.outs['Z' + str(self.num_layers - 1)] = self.sigmoid(a)
 
-            # OUTPUT LAYER
-            # check these two lines below when generalizing
-            Z1 = np.reshape(Z1, (1, len(Z1)))
-            layer_in = np.ones((1,1))
-            layer_in = np.hstack((layer_in, Z1)).T
+                # backward
+                # compute deltas
+                labels = self.train_labels[:,j].reshape(self.num_classes, 1)
+                delta_out = np.multiply(np.subtract(self.outs['Z' + str(self.num_layers - 1)], labels), self.outs['Z' + str(self.num_layers - 1)])
+                delta_out = np.multiply(delta_out, 1 - self.outs['Z' + str(self.num_layers - 1)])
+                delta = {}
+                delta = {'D' + str(self.num_layers - 1) : delta_out}
+                
+                for x in range(self.num_layers - 2, 0, -1):
+                    right = np.multiply(self.outs['Z' + str(x)], 1 - self.outs['Z' + str(x)])
+                    weights = self.model['W' + str(x + 1)]
+                    delt = delta['D' + str(x + 1)]
+                    delt = delt if x == self.num_layers - 2 else delt[1:]
+                    delta['D' + str(x)] = np.multiply(np.dot(delt.T, weights.T).T, right)
 
-            a = np.dot(W2.T, layer_in)
-            Z2 = self.sigmoid(a)
-            self.outs['Z2'] = Z2
-            print('After hidden layer -> output feedforward')
-            print(layer_in)
-            print(W2)
-            print(a)
-            print(Z2)
+                # update weights
 
-            # BACKWARDS
-            # OUTPUT LAYER
-            labels = self.train_labels[:,0].reshape((10,1))
-            delta_out = np.multiply(np.subtract(self.outs['Z2'], labels), self.outs['Z2']) 
-            delta_out = np.multiply(delta_out, (1 - self.outs['Z2']))
-            print('Delta of output layer')
-            print(delta_out)
-
-            # HIDDEN LAYER
-            # need to exclude bias units
-            right = np.multiply(self.outs['Z1'], 1 - self.outs['Z1'])
-            delta_hidden = np.multiply(np.sum(np.multiply(delta_out, self.model['W2'][1])), right)
-            print('Delta of hidden layer')
-            print(right)
-            print(delta_hidden)
-
-            # UPDATE
-            # INNER LAYER
-            print('Update inner -> hidden')
-            print(self.model['W1'])
-            update_weightLayer1 = np.multiply(self.learning_rate * delta_hidden, self.train_data[:,0])
-            self.model['W1'] = np.subtract(self.model['W1'].T, update_weightLayer1).T
-            print(update_weightLayer1)
-            print(self.model['W1'])
-
-            # HIDDEN LAYER
-            print('Update hidden -> output')
-            print(self.model['W2'])
-            update_weightLayer2 = np.multiply(self.learning_rate * delta_out, self.outs['Z1'])
-            self.model['W2'][1] = np.subtract(self.model['W2'][1], update_weightLayer2.T).T.reshape(10,)
-            print(update_weightLayer2)
-            print(self.model['W2'])
+                for j in range(1, self.num_layers):
+                    if j > 0 and j < self.num_layers - 1:
+                        d = delta['D' + str(j)][1:]
+                    else:
+                        d = delta['D' + str(j)]
+                    gradient = self.learning_rate * d * self.outs['Z' + str(j - 1)].T
+                    self.model['W' + str(j)] = self.model['W' + str(j)] - gradient.T
 
             self.learning_rate *= .98
 
-        print(self.outs['Z2'])
 
-
-    def test(self, data, labels):
-        print('TESTING')
-        print('TESTING')
-        print('TESTING')
-        print('TESTING')
+    def test(self, data):
         data = np.divide(data, self.max_normalize)
         z = data.T
 
         for i in range(1, self.num_layers):
             temp = np.ones((len(z[0]), 1)).T
             z = np.vstack((temp, z))
-    
             a = np.dot(self.model['W' + str(i)].T, z) 
             z = self.sigmoid(a)
 
         return z
+
 
     def sigmoid(self, val):
         temp = np.exp(np.negative(val))
--- fall2018/4309/hw3/neural_network.py
+++ fall2018/4309/hw3/neural_network.py
@@ -13,10 +13,11 @@ from file_utility import FileUtility
 from network import NeuralNetwork
 
 
-def validate(arg_name: str, arg: int, val: int):
+def validate(arg_name: str, arg: int, val: int) -> int:
     if arg < val:
         print("argument '%s' must be greater than %d" % (arg_name, val - 1))
         quit()
+    return arg
 
 
 if len(sys.argv) != 6:
@@ -26,13 +27,9 @@ if len(sys.argv) != 6:
 training_filename = sys.argv[1]
 test_filename = sys.argv[2]
 
-layers = int(sys.argv[3])
-units_per_layer = int(sys.argv[4])
-rounds = int(sys.argv[5])
-
-validate("layers", layers, 2)
-validate("units_per_layer", units_per_layer, 1)
-validate("rounds", rounds, 1)
+layers = validate("layers", int(sys.argv[3]), 2)
+units_per_layer = validate("units_per_layer", int(sys.argv[4]), 1)
+rounds = validate("rounds", int(sys.argv[5]), 1)
 
 
 # find the file in the system with the specified filename
@@ -50,16 +47,16 @@ training_data, training_classes, test_data, test_classes = file_util.get_dataset
 
 neural_network = NeuralNetwork(layers, units_per_layer, rounds)
 neural_network.initialize_training(training_data, training_classes)
-neural_network.train()
-
-predictions = neural_network.test(test_data, test_classes)
+neural_network.train_general()
+predictions = neural_network.test(test_data)
 total_accuracy = 0
 for i in range(0, len(test_data)):
-    prediction = np.argmax(predictions[:,i], axis = 0) + 1
-    count = np.count_nonzero(predictions[:,i] == predictions[prediction][i])
-    acc = 1 / count if prediction == test_classes[i] else 0
-
+    prediction_index = np.argmax(predictions[:,i], axis = 0)
+    count = np.count_nonzero(predictions[:,i] == predictions[prediction_index][i])
+    acc = 1 / count if np.unique(test_classes)[prediction_index] == test_classes[i] else 0
     total_accuracy += acc
-    print('ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f\n' % (i + 1, prediction, test_classes[i], acc))
+
+    print('ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f\n' % (i + 1, np.unique(test_classes)[prediction_index], test_classes[i], acc))
 
 print('classification accuracy=%6.4f\n' % (total_accuracy / len(test_data)))
+print(np.unique(test_classes))
