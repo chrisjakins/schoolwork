936c236325a380deef00caa3608b93553c0c0bfc
chrisjakins <chris.fp.jakins@gmail.com>
2019-03-22 10:05:19 -0500

    Final commit of comp vision hw4

--- spring2019/CSE4310/assignment4/directed_chamfer.m
+++ spring2019/CSE4310/assignment4/directed_chamfer.m
@@ -4,24 +4,7 @@ function distance = directed_chamfer(im_from, im_to);
 %   Images asssumed to be logical, binary images.
 %
 
-% [from_row, from_col] = size(im_from);
-% [to_row, to_col] = size(im_to);
-% 
-% num_conn = 0;
-% 
-% for j = 1:from_col
-%     for i = 1:from_row
-%         if im_from(i,j) == 1
-%         
-%         
-%         
-%         
-%         end
-%     end
-% end
-im1 = double(im_from);
-im2 = double(im_to);
-
-dt = bwdist(im2);
-
-distance = sum(sum(im1 .* dt));
+from_bin = (im_from ~= 0);
+n1 = sum(sum(from_bin));
+dt2 = bwdist(im_to);
+distance = sum(sum(from_bin .* dt2)) / n1;
