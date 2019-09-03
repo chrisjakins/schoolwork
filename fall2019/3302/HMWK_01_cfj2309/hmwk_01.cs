05b52ac9abdcc67e0953f36e3aac51092ccecde7
Chris <chris.fp.jakins@gmail.com>
2019-09-03 16:50:10 -0500

    Initial commit of Programming Languages 3302, hw1

---
+++ fall2019/3302/HMWK_01_cfj2309/hmwk_01.cs
@@ -0,0 +1,13 @@
+// Jakins, Christopher
+// cfj2309
+// 2019-09-03
+
+using System;
+
+public class hmwk_01
+{
+  static public void Main( string[] args )
+  {
+      Console.WriteLine("Hello, World!");
+  }
+}
---
+++ fall2019/3302/HMWK_01_cfj2309/hmwk_01.java
@@ -0,0 +1,10 @@
+// Jakins, Christopher
+// cfj2309
+// 2019-09-03
+
+public class hmwk_01 {
+  public static void main( String[] args )
+  {
+      System.out.println("Hello, World!");
+  }
+}
