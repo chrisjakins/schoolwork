ef61d0e37fa416b3a70bdac85ef415952996313e
Chris Jakins <cjakins12@yahoo.com>
2019-09-30 17:00:27 -0500

    Initial multithreaded server commit

---
+++ fall2019/4344/hw1/Server.java
@@ -0,0 +1,50 @@
+package hw1;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
+class Server implements Runnable {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private int port_ = 0;
+    private Thread runningThread = null;
+
+    public Server(int port) {
+        this.port_ = port;
+    }
+
+    public void run() {
+        System.out.println("Running one");
+        synchronized(this) {
+            this.runningThread = Thread.currentThread();
+        }
+        try {
+            this.server = new ServerSocket(this.port_);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        while (true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.server.accept();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
+            new Thread(new WorkerRunnable(clientSocket, "Server")).start();
+        }
+    }
+
+    private void handleRequest(String request) {
+    
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Server server = new Server(8080);
+
+        new Thread(server).start();
+    }
+}
---
+++ fall2019/4344/hw1/WorkerRunnable.java
@@ -0,0 +1,30 @@
+package hw1;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
+public class WorkerRunnable implements Runnable {
+
+    protected Socket clientSocket = null;
+    protected String serverText = null;
+
+    public WorkerRunnable(Socket clientSocket, String serverText) {
+        this.clientSocket = clientSocket;
+        this.serverText   = serverText;
+    }
+
+    public void run() {
+        try {
+            InputStream input = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+            long time = System.currentTimeMillis();
+            output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " + this.serverText + " - " + time + "").getBytes()); output.close(); input.close();
+            System.out.println("Request processed: " + time);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
