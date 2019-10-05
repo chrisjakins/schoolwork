4534f30653665bbb170b34be00fc8f0c7951944c
Chris Jakins <cjakins12@yahoo.com>
2019-10-05 15:20:49 -0500

    Hw3 for 3302

---
+++ fall2019/3302/HMWK_03_cfj2309/hmwk_03.c
@@ -0,0 +1,12 @@
+// Jakins, Christopher
+// cfj2309
+// 2019-10-05
+
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+  // Put an output statement here that prints "Hello, world!"
+  // followed by a newline.
+  printf("Hello, world!\n");
+}
---
+++ fall2019/3302/HMWK_03_cfj2309/hmwk_03.cc
@@ -0,0 +1,14 @@
+// Jakins, Christopher
+// cfj2309
+// 2019-10-05
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char *argv[] )
+{
+  // Put an output statement here that prints "Hello, world!"
+  // followed by a newline.
+    std::cout << "Hello, world!" << std::endl;
+}
